0aef0e2912c2cade053911dabbe1e49163426fd1
you@example.com <you@example.com>
2025-04-24 15:02:48 +0300

    testing routes and views

---
+++ resources/views/test.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            text-align: center;
+            background-color: lightcyan;
+        }
+    </style>
+</head>
+
+<body>
+    {{ $variable }}
+    <h1>About me</h1>
+    <ul>
+        <li>Lorem. ipsum dolor sit, amet consectetur adipisicing elit. Neque perferendis molestias temporibus pariatur mollitia eum eius nostrum cumque veritatis, facilis rerum ratione consequatur nisi atque voluptatum fugiat numquam natus eaque. ipsum dolor sit amet consectetur, adipisicing elit. Dolore cum iste cupiditate ad tenetur hic expedita aperiam quisquam! Sapiente vero earum modi, molestiae ducimus veritatis tempora ex possimus voluptate corporis.</li>
+        <li>Lorem, ipsum. ipsum dolor sit, amet consectetur adipisicing elit. Neque perferendis molestias temporibus pariatur mollitia eum eius nostrum cumque veritatis, facilis rerum ratione consequatur nisi atque voluptatum fugiat numquam natus eaque. ipsum dolor sit amet consectetur, adipisicing elit. Dolore cum iste cupiditate ad tenetur hic expedita aperiam quisquam! Sapiente vero earum modi, molestiae ducimus veritatis tempora ex possimus voluptate corporis.</li>
+        <li>Lorem, ipsum dolor. ipsum dolor sit, amet consectetur adipisicing elit. Neque perferendis molestias temporibus pariatur mollitia eum eius nostrum cumque veritatis, facilis rerum ratione consequatur nisi atque voluptatum fugiat numquam natus eaque. ipsum dolor sit amet consectetur, adipisicing elit. Dolore cum iste cupiditate ad tenetur hic expedita aperiam quisquam! Sapiente vero earum modi, molestiae ducimus veritatis tempora ex possimus voluptate corporis.</li>
+    </ul>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/test', function () {
+//     // return view('welcome');
+// });
+
+
+
+Route::get('/about', function () {
+    $name = "Nichuks";
+    echo "<h1>" . $name;
+    return view('test', ['variable' => "my variable text value"]);
+});
+
+Route::get('/posts', [PostController::class, 'index']);
